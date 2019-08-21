4b3d286dabefe8892d0561524974786ee7887aa1
Harish Naik <harishnaik.bs@aceturtle.com>
2019-08-21 21:30:51 +0530

    Updates

---
+++ precache-manifest.3d405a9df97507de721fdf31153782d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dd4b522ad37e009d905a6f98959084",
+    "url": "/react_rubicon/index.html"
+  },
+  {
+    "revision": "7990896984e286e388c6",
+    "url": "/react_rubicon/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "374637c152da947e7af9",
+    "url": "/react_rubicon/static/css/main.94920682.chunk.css"
+  },
+  {
+    "revision": "7990896984e286e388c6",
+    "url": "/react_rubicon/static/js/2.df980a0d.chunk.js"
+  },
+  {
+    "revision": "374637c152da947e7af9",
+    "url": "/react_rubicon/static/js/main.ee82c2a8.chunk.js"
+  },
+  {
+    "revision": "a6a5dff469bd65beb834",
+    "url": "/react_rubicon/static/js/runtime~main.f27ca9f1.js"
+  },
+  {
+    "revision": "0ca65cc6a7b195aa5e013a6ff2b48544",
+    "url": "/react_rubicon/static/media/Catalog_Master_Icon.0ca65cc6.svg"
+  },
+  {
+    "revision": "aeaef67c6affd6bd6b3be743278c42bc",
+    "url": "/react_rubicon/static/media/login_bg.aeaef67c.jpg"
+  },
+  {
+    "revision": "1921eb67a0627045e5eb17ae311d60a3",
+    "url": "/react_rubicon/static/media/rxjavalogo.1921eb67.png"
+  }
+]);
